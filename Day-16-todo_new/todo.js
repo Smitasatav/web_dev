15a20d1ccdfe4191be5c6fdf4c6b40acf5f7f53d
Smita Satav <smitasatav0134@gmail.com>
2024-01-23 17:41:44 +0530

    add changes

---
+++ Day-16-todo_new/todo.js
@@ -0,0 +1,33 @@
+let tasks = [];
+let add = () => {
+  let title = document.getElementById("title").value;
+  let detail = document.getElementById("detail").value;
+  tasks.push({ title, detail });
+  render();
+  document.getElementById("title").value = "";
+  document.getElementById("detail").value = "";
+  document.getElementById("customers").style.display = "block";
+  document.getElementById("form").style.display = "none";
+};
+let showForm = () => {
+  document.getElementById("customers").style.display = "none";
+  document.getElementById("form").style.display = "block";
+};
+
+let render = () => {
+  let html = "";
+  tasks.forEach((d, i) => {
+    html += `<tr>
+                    <td>${i + 1}</td>
+                    <td>${d.title}</td> 
+                    <td>${d.detail}</td> 
+                    <td><button onclick="deleteTask(${i})">X</button></td>
+             </tr>`;
+  });
+  document.getElementById("task-table").innerHTML = html;
+};
+
+let deleteTask = (i) => {
+  tasks.splice(i, 1);
+  render();
+};

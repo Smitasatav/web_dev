f2e1707a19b5c8309a8382163da06948a633a8f4
Smita Satav <smitasatav0134@gmail.com>
2024-01-23 17:40:47 +0530

    add new code

--- Day_15-multiplication_table/practice_mul.js
+++ Day_15-multiplication_table/practice_mul.js
@@ -1,17 +1,15 @@
 const multiply = () => {
   const input1 = parseInt(document.getElementById("input1").value);
   const input2 = parseInt(document.getElementById("input2").value);
-
   let arr = [];
   for (var i = 1; i <= input2; i++) {
     arr.push({ number: i, total: i * input1 });
   }
-
   let html = "";
   arr.forEach((d) => {
-    html += `<tr><td>${input1}</td> 
-             <td>${d.number}</td> 
-             <td>${d.total}</td></tr>`;
+    html += `<tr><td>${input1}</td>
+           <td>${d.number}</td>
+           <td>${d.total}</td></tr>`;
   });
   document.getElementById("mul_table").innerHTML = html;
 };
